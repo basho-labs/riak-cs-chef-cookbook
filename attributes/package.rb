44da980a7cf9fe967c082a1b542a0911b8577872
Seth Thomas <seth.g.thomas@gmail.com>
2012-09-02 13:21:21 -0500

    Updating Copyright date

--- attributes/package.rb
+++ attributes/package.rb
@@ -2,7 +2,7 @@
 # Author::Sean Carey (<densone@basho.com>)
 # Cookbook Name:: cs
 #
-# Copyright (c) 2011 Basho Technologies, Inc.
+# Copyright (c) 2012 Basho Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
